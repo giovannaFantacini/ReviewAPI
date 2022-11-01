5cf4c8239f05a33278706ba11e982e22c8b7f3ac
1201441BrunoPereira <1201441@isep.ipp.pt>
2022-11-01 14:56:40 +0000

    Atualização JwtRequestFilter para obtenção de Role

--- src/main/java/com/Review/ReviewAPI/security/JwtRequestFilter.java
+++ src/main/java/com/Review/ReviewAPI/security/JwtRequestFilter.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,7 +15,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
+
 
 public class JwtRequestFilter extends OncePerRequestFilter {
     @Autowired
@@ -31,13 +30,16 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
         final String requestTokenHeader = request.getHeader("Authorization");
 
-        String username = null;
+        String userId = null;
         String jwtToken = null;
+        String role = null;
 
         if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
             jwtToken = requestTokenHeader.substring(7);
             try {
-                username = jwtUtils.getUserFromJwtToken(jwtToken);
+                userId = jwtUtils.getUserFromJwtToken(jwtToken);
+                role = jwtUtils.getAuthorityFromJwtToken(jwtToken);
+                role = role.substring(1,role.length()-1);
                 jwtUtils.setJwt(jwtToken);
             } catch (IllegalArgumentException e) {
                 System.out.println("Unable to get JWT Token");
@@ -49,11 +51,11 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             logger.warn("JWT Token does not begin with Bearer String");
         }
 
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
             if (jwtUtils.validateJwtToken(jwtToken)) {
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                        username, null, AuthorityUtils.createAuthorityList("REG_USER"));
+                        userId, null, AuthorityUtils.createAuthorityList(role));
                 usernamePasswordAuthenticationToken
                         .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
